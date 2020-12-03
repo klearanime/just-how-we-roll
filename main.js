b1d42a76064f3294ef72ea7c61236f0b7f24e92a
Crystal K Young <maybiz@hotmail.com>
2020-12-03 13:26:57 -0500

    mode-function-added

--- main.js
+++ main.js
@@ -27,86 +27,98 @@ const getRandomNumber = function(max) {
   }
   
   
-  /****************
-   * MATH SECTION *
-   ****************/
+  /***************
+   *   Queries   *
+   ***************/
   
-  const mean = function (array) {
-    let average = 0
-    let sum = 0
-    let arrLength = array.length
-    for (number of array) {
-      sum = sum + number
-      average = sum / arrLength
+  const d6Die = document.querySelector('#d6-roll')
+  d6Die.src = 'images/start/d6.png'
+  
+  const d6Die1 = document.querySelector('#double-d6-roll-1')
+  d6Die1.src = './images/start/d6.png'
+  
+  const d6Die2 = document.querySelector('#double-d6-roll-2.d6.roll')
+  d6Die1.src = './images/start/d6.png'
+  
+  const d12Die = document.querySelector('#d12-roll.roll')
+  d12Die.src = './images/start/d12.jpeg'
+  
+  const d20Die = document.querySelector('#d20-roll.roll')
+  d20Die.src = './images/start/d20.jpg'
+  
+  const resetButton = document.querySelector('#reset-button') 
+  
+  const mean6 = document.querySelector('#d6-rolls-mean')
+  const median6 = document.querySelector('#d6-rolls-median')
+  const mode6 = document.querySelector('#d6-rolls-mode')
+  
+  const d6Mean = document.querySelector('#double-d6-rolls-mean')
+  const d6Median = document.querySelector('#double-d6-rolls-median')
+  const d6Mode = document.querySelector('#double-d6-rolls-mode')
+  
+  const d12Mean = document.querySelector('#d12-rolls-mean')
+  const d12edian = document.querySelector('#d12-rolls-median')
+  const d12Mode = document.querySelector('#d12-rolls-mode')
+  
+  const d20Mean = document.querySelector('#d12-rolls-mean')
+  const d20Median = document.querySelector('#d12-rolls-median')
+  const d20Mode = document.querySelector('#d12-rolls-mode')
+  
+  
+    /****************
+     * MATH SECTION *
+     ****************/
+    
+    const mean = function (array) {
+      let average = 0
+      let sum = 0
+      let arrLength = arr.length
+      for (const num of array) {
+        sum = sum + num
+        average = sum / arrLength
+      }
+      rollResult.push(average)
+      return (average)
     }
-    rollResult.push(average)
-    return (average)
-  }
+    
+    
+    const median = function (array) {
+      array = sortByNumber(arr)
+      let middleCharacter = Math.floor(array.length / 2)
+      rollResult.push(middleCharacter)
+      return (middleCharacter)
+    }
+    
+    
+    const mode = function (rolls) { 
+      const counts = []
+      let mode = "NA"
+      for (const roll of rolls) {
+        if (roll in counts) {
+          counts[roll]++
+        } else {
+          counts[roll] = 1
+        }
+
+        if (counts[mode] === undefined || counts[roll] > counts[mode]) {
+          mode = roll
+        }
+      }
+
+      return mode
+    }
   
-  
-  const median = function (array) {
-    array = sortByNumber(array)
-    let middleCharacter = Math.floor(array.length / 2)
-    rollResult.push(middleCharacter)
-    return (middleCharacter)
-  }
-  
-  
-  const mode = function () { 
-  
-  }
-
-
-/***************
- *   Queries   *
- ***************/
-
-const d6Die = document.querySelector('#d6-roll')
-d6Die.src = 'images/start/d6.png'
-
-const d6Die1 = document.querySelector('#double-d6-roll-1')
-d6Die1.src = './images/start/d6.png'
-
-const d6Die2 = document.querySelector('#double-d6-roll-2.d6.roll')
-d6Die2.src = './images/start/d6.png'
-
-const d12Die = document.querySelector('#d12-roll.roll')
-d12Die.src = './images/start/d12.jpeg'
-
-const d20Die = document.querySelector('#d20-roll.roll')
-d20Die.src = './images/start/d20.jpg'
-
-const resetButton = document.querySelector('#reset-button') 
-
-const mean6 = document.querySelector('#d6-rolls-mean')
-const median6 = document.querySelector('#d6-rolls-median')
-const mode6 = document.querySelector('#d6-rolls-mode')
-
-const d6Mean = document.querySelector('#double-d6-rolls-mean')
-const d6Median = document.querySelector('#double-d6-rolls-median')
-const d6Mode = document.querySelector('#double-d6-rolls-mode')
-
-const d12Mean = document.querySelector('#d12-rolls-mean')
-const d12edian = document.querySelector('#d12-rolls-median')
-const d12Mode = document.querySelector('#d12-rolls-mode')
-
-const d20Mean = document.querySelector('#d12-rolls-mean')
-const d20Median = document.querySelector('#d12-rolls-median')
-const d20Mode = document.querySelector('#d12-rolls-mode')
-
-
-/*******************
- * YOUR CODE BELOW *
+  /*******************
+   * YOUR CODE BELOW *
  *******************/
 
-// for image recognition in functions
 let numbersImgSource = './images/numbers/'
-
 let rollResult = []
 
 const dice6Roll = function () {
   let result = getRandomNumber(6);
   let png = d6Die + `${result}.png`
+  sixes.push(result)
   d6Die.src = png
   mean6.innerText = mean(rollResult)
   median6.innerText = median(rollResult)
@@ -125,25 +137,23 @@ const double6Dice1Roll = function () {
 
 const double6Dice2Roll = function () {
   let result = getRandomNumber(6);
-  let pngImage = d6Die + `${result}.png`
-  d6Die2.src = pngImage
+  let png = d6Die + `${result}.png`
+  d6Die2.src = png
   rollResult.push(result)
 }
 
 
 const twelvesDiceRoll = function () {
   let result = getRandomNumber(12)
-  let pngImage = numbersImgSource + `${result}.png`
-  d12Die.src = pngImage
+  let png = numbersImgSource + `${result}.png`
+  d12Die.src = png
 }
 
 
 const die20Roll = function () {
   let result = getRandomNumber(20)
-
-  let pngImg = numbersImgSource + `${result}.png`
-
-  d20Die.src = pngImg
+  let png = numbersImgSource + `${result}.png`
+  d20Die.src = png
 }
 
 
@@ -156,6 +166,12 @@ const reset = function () {
   doubleSixes.length = 0;
   twelves.length = 0;
   twenties.length = 0;
+
+  d6Die.src = 'images/start/d6.png'
+  d6Die1.src = './images/start/d6.png'
+  d6Die2.src = './images/start/d6.png'
+  d12Die.src = './images/start/d12.jpeg'
+  d20Die.src = './images/start/d20.jpg'
 
   //  reset text in mean median mode text
   mean6.innerText = ''
